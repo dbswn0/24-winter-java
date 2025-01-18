96511134c671de51334b98b9ddb420faf0edcd96
정윤주 <oyunjooya@naver.com>
2025-01-19 00:09:06 +0900

    커스텀 구분자로 문자열 분리

--- level2/[level2] 정윤주/application/src/main/java/org/example/Application.java
+++ level2/[level2] 정윤주/application/src/main/java/org/example/Application.java
@@ -20,17 +20,34 @@ public class Application {
     //기본 구분자로 문자열 분리
     public static ArrayList<Integer> splitBasic(String str) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        String[] tokens = str.split("[,:]");
+        String[] parts = str.split("[,:]");
 
-        for (String token : tokens) {
-            try {
-                int number = Integer.parseInt(token); // 숫자로 변환
-                numbers.add(number); //리스트에 추가
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("잘못된 값 입력");
-            }
+        for (String part : parts) {
+            int number = Integer.parseInt(part); // 숫자로 변환
+            numbers.add(number); //리스트에 추가
         }
         return numbers;
+    }
+
+    //커스텀 구분자로 문자열 분리
+    public static ArrayList<Integer> splitCustom(String str) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        String customDelimiter = String.valueOf(",");
+        String numbersPart = str;
+
+        // 커스텀 구분자 확인
+        if (str.startsWith("//") && str.contains("\n")) {
+            int lastDelimiter = str.indexOf('\n');
+            customDelimiter = str.substring(2, lastDelimiter); // 구분자 추출
+            numbersPart = str.substring(lastDelimiter + 1); // 숫자 부분
+        }
+
+        String[] parts = numbersPart.split(customDelimiter);
+
+        for (String part : parts) {
+            numbers.add(Integer.valueOf(part));
+        }
+        return numbers; // 숫자 리스트 반환
     }
 
     public static int calculate(ArrayList<Integer> numbers) {
