47d34228ce49c919e903091e867e36cbc57dce70
정윤주 <oyunjooya@naver.com>
2025-01-22 19:36:36 +0900

    [View] 입력 받기

---
+++ level2/[level2] 정윤주/application/src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import java.util.Scanner;
+
+public class CalculatorView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return scanner.nextLine();
+    }
