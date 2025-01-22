f0618b92c1cbc7f3a29752f5a9d6df9369093131
정윤주 <oyunjooya@naver.com>
2025-01-22 19:40:44 +0900

    [View] 결과 출력

--- level2/[level2] 정윤주/application/src/main/java/calculator/view/CalculatorView.java
+++ level2/[level2] 정윤주/application/src/main/java/calculator/view/CalculatorView.java
@@ -9,3 +9,12 @@ public class CalculatorView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return scanner.nextLine();
     }
+
+    public void displayResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
+}
