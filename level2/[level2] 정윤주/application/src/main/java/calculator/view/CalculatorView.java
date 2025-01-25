4511a27198f4ae11df5a002163227303c6092ad8
정윤주 <oyunjooya@naver.com>
2025-01-25 21:55:30 +0900

    [level2]2주차 정윤주 학습 PR 제출합니다.

--- level2/[level2] 정윤주/application/src/main/java/calculator/view/CalculatorView.java
+++ level2/[level2] 정윤주/application/src/main/java/calculator/view/CalculatorView.java
@@ -7,7 +7,7 @@ public class CalculatorView {
 
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return scanner.nextLine();
+        return scanner.nextLine(); // 한 줄 입력 받기
     }
 
     public void displayResult(int result) {
